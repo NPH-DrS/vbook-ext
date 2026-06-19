2d23edfe030762571f8954d838c350b68a8b16ae
NPH-DrS <nph.drs.0901@gmail.com>
2026-06-19 23:31:29 +0700

    Fix typo in cosplay video category input path

--- cosplaytele/src/home.js
+++ cosplaytele/src/home.js
@@ -1,6 +1,6 @@
 function execute() {
     return Response.success([
         {title: "Trang chủ", input: "", script: "gen.js"},
-        {title: "Cosplay video", input: "category/video-cosplay/", script: "gen.js"},
+        {title: "Cosplay video", input: "category/video-cosplayy/", script: "gen.js"},
     ]);
 }
